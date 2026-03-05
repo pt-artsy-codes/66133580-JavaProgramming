df8f432dbd643322be9812cf54f5348080641398
pt-artsy-codes <phuthinhnguyen9@gmail.com>
2026-03-05 12:54:47 +0700

    Updated a few lines of codes

--- USD_VND_Money_Converter/src/edu/USD_VND_Converter.java
+++ USD_VND_Money_Converter/src/edu/USD_VND_Converter.java
@@ -16,16 +16,18 @@ public class USD_VND_Converter {
         System.out.print("Choice: ");
         int choice = scanner.nextInt();
 
-        System.out.print("Nhập số tiền: ");
-        double amount = scanner.nextDouble();
-
         double kq = 0;
+        
         if (choice == 1) {
+        	System.out.print("Nhập số tiền (đơn vị: 1$): ");
+            double amount = scanner.nextDouble();
             kq = amount * Convert;
-            System.out.printf("%.2f USD = %.2f VND%n", amount, kq);
+            System.out.printf("%.2f USD = %.0f VND%n", amount, kq);
         } else if (choice == 2) {
-            kq = amount /Convert;
-            System.out.printf("%.2f VND = %.2f USD%n", amount, kq);
+        	System.out.print("Nhập số tiền (đơn vị: 1.000 VND): ");
+            double amount = scanner.nextDouble();
+            kq = (amount * 1000) / Convert;
+            System.out.printf("%.3f VND = %.2f USD%n", amount, kq);
         } else {
             System.out.println("Lựa chọn không hợp lệ.");
         }

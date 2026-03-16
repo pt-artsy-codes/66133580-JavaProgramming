ded5423c97f3ed9da9cbde8953a3c98499f93d86
pt-artsy-codes <phuthinhnguyen9@gmail.com>
2026-03-16 23:07:25 +0700

    thêm chức năng in ra kết quả cùng với 1 số sửa đổi nhỏ

--- ViduArrayListSoNguyen/src/edu/runMain.java
+++ ViduArrayListSoNguyen/src/edu/runMain.java
@@ -8,10 +8,10 @@ public class runMain {
 	public static void main(String[] args) {
 		//Khai báo biến danh sách
 		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
-		//Cau a: Nhậpp 10 phần tử từ bàn phím
+		//Cau a: Nhập 10 phần tử từ bàn phím
 		Scanner banPhim = new Scanner(System.in);
 		
-		for (int i=0; i<10; i++) {
+		for (int i=1; i<=10; i++) {
 			//Nhập 1 phần tử
 				//in ra hướng dẫn cho người dùng
 			System.out.println("Nhập các phần tử thứ " +i + ": ");
@@ -20,7 +20,17 @@ public class runMain {
 				//thêm vào trong danh sách
 			dsSoNguyen.add(tam);	
 		}
-			
+		
+		//Câu b: in ra danh sách vừa nhập
+		System.out.println("Danh sách vừa nhập là: \n");
+//		for (int i=0; i<dsSoNguyen.size(); i++) {
+//			Integer tam = dsSoNguyen.get(i);
+//			System.out.println(tam + " ");
+//		}
+		
+		for (Integer x: dsSoNguyen) {
+			System.out.print(x + " ");
+		}
 	}
 
 }

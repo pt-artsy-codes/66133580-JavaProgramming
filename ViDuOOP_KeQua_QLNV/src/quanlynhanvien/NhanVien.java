72a3d73df1a150cd68d928e6adddaf09f5fba8f6
pt-artsy-codes <phuthinhnguyen9@gmail.com>
2026-03-19 09:19:06 +0700

    Viết hàm main để test lớp nhân viên

--- ViDuOOP_KeQua_QLNV/src/quanlynhanvien/NhanVien.java
+++ ViDuOOP_KeQua_QLNV/src/quanlynhanvien/NhanVien.java
@@ -1,5 +1,35 @@
 package quanlynhanvien;
 
 public class NhanVien {
-
+	//thuộc tính
+	private String maNV;
+	private String hoTen;
+	private double luongCoBan;
+	
+	//hàm tạo
+	public NhanVien(String maNV, String hoTen, double luongCoBan) {
+		super();
+		this.maNV = maNV;
+		this.hoTen = hoTen;
+		this.luongCoBan = luongCoBan;
+	}
+	public NhanVien() {
+		super();
+	}
+	@Override
+	public String toString() {
+		String chuoiXuat="";
+		chuoiXuat = chuoiXuat + "Mã NV: " + this.maNV;
+		chuoiXuat = chuoiXuat + "\nHọ tên: " + this.hoTen;
+		chuoiXuat = chuoiXuat + "\nLương cơ bản: " + this.luongCoBan;
+		return chuoiXuat;
+		
+//		return "NhanVien [maNV=" + maNV + ", hoTen=" + hoTen + ", luongCoBan=" + luongCoBan + "]";
+	}
+	
+	public void hienThiThongTin() {
+		System.out.println("Mã Nv: " + maNV);
+		System.out.println("Họ tên: " + hoTen);
+		System.out.println("Lương cơ bản: " + luongCoBan);
+	}
 }
---
+++ ViDuOOP_KeQua_QLNV/src/quanlynhanvien/Run.java
@@ -0,0 +1,11 @@
+package quanlynhanvien;
+
+public class Run {
+
+	public static void main(String[] args) {
+		NhanVien nv1 = new NhanVien("001", "Nguyễn Phú Thịnh", 2300000);
+//		nv1.hienThiThongTin();
+		System.out.print(nv1.toString());
+	}
+
+}

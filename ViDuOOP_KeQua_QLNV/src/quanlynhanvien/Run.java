2e85c91fdb56a29a7bce56c556a11560d563eeb3
pt-artsy-codes <phuthinhnguyen9@gmail.com>
2026-03-19 09:48:33 +0700

    Xong lớp nhân viên văn phòng + test code

--- ViDuOOP_KeQua_QLNV/src/quanlynhanvien/Run.java
+++ ViDuOOP_KeQua_QLNV/src/quanlynhanvien/Run.java
@@ -5,7 +5,11 @@ public class Run {
 	public static void main(String[] args) {
 		NhanVien nv1 = new NhanVien("001", "Nguyễn Phú Thịnh", 2300000);
 //		nv1.hienThiThongTin();
-		System.out.print(nv1.toString());
+		System.out.println(nv1.toString());
+		
+		NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("002", "Michael Reeves", 4200000, 3, 400000);
+		System.out.println(nvVanPhong1.toString());
+//		nvVanPhong1.hienThiThongTin();
 	}
 
 }
